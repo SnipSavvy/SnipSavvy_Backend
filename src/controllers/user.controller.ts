a42f62d0f91cf6e93d33dce1dbc55b1dec146775
Sahil <sahilaroraji2002@gmail.com>
2024-05-11 21:44:57 +0530

    [FIX] Handling Email Service - only when new user created

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -15,15 +15,15 @@ export async function register(req: Request, res: Response) {
     user = await IS_USER_PRESENT(email);
     if (!user) {
       user = await CREATE_USER(req.body);
+      await emailService(
+          email,
+          "Welcome to SnipSavvy",
+          { name: user.name },
+          register_user
+        )
+          .then(() => logger.info(`Email sent successfully to ${email}`))
+          .catch((error) => logger.error("Error in sending email:", error));
     }
-  await emailService(
-      email,
-      "Welcome to SnipSavvy",
-      { name: user.name },
-      register_user
-    )
-      .then(() => logger.info(`Email sent successfully to ${email}`))
-      .catch((error) => logger.error("Error in sending email:", error));
     const token = await CREATE_JWT(user);
 
     logger.info(`token get in controller => ${token}`);
