3ff7ed2431b80fc990a7efe09fd0aa1131f076f5
YASH-RAJ-HANS <yashhans479@gmail.com>
2024-05-12 20:03:19 +0530

    [API] category update api implemented

--- src/Routes/category.route.ts
+++ src/Routes/category.route.ts
@@ -3,6 +3,7 @@ import {
   createCategory,
   deleteCategories,
   fetchCategoriesByWorkspace,
+  updateCategory,
 } from "../controllers/category.controller";
 const categoryRouter: Router = express.Router();
 const authMiddleware = require("../middlewares/auth.middleware");
@@ -15,3 +16,4 @@ categoryRouter.get(
 );
 categoryRouter.delete("/", authMiddleware, deleteCategories);
 module.exports = categoryRouter;
+categoryRouter.put("/:workspace_id/:category_id",updateCategory)
